6fa13ae50fa98deb32ca7f6c84cda0733d345c88
myogaazwar <yogaazwar123@gmail.com>
2024-11-03 17:53:51 +0700

    added: props value, onclick, disabled for button

--- src/components/Elements/Button/Button.jsx
+++ src/components/Elements/Button/Button.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
-const Button = ({ children, classname }) => {
+const Button = ({ children, classname, value, onClick, disabled }) => {
   return (
-    <button className={`${classname}  w-full h-10 rounded-md`}>
+    <button
+      className={`${classname}  w-full h-10 rounded-md`}
+      value={value}
+      onClick={onClick}
+      disabled={disabled}
+    >
       {children}
     </button>
   );
