2a94c240f06dafe9cfa48279a1caffa9772ce58d
myogaazwar <yogaazwar123@gmail.com>
2024-11-05 20:08:30 +0700

    setup page transaction

--- src/pages/Transaction.jsx
+++ src/pages/Transaction.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const Transaction = () => {
-  return <main className='mt-14'>Halaman Transaksi</main>;
+  return (
+    <main className='mt-14'>
+      <h2>Semua Transaksi</h2>
+
+      <section>
+        <div></div>
+      </section>
+    </main>
+  );
 };
 
 export default Transaction;
