0f6c3e6450c4d7bf66289429bcb00ec2b4ab6eb2
myogaazwar <yogaazwar123@gmail.com>
2024-11-09 07:27:00 +0700

    update header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,26 +1,13 @@
-import React, { useContext, useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../assets/WebsiteAssets/Logo.png';
-import profileImageDefault from '../../assets/WebsiteAssets/Profile Photo.png';
 
-import { FaEye, FaEyeSlash } from 'react-icons/fa';
 import { GiHamburgerMenu } from 'react-icons/gi';
 
 import NavItem from './NavItem';
-import { UserBalanceContext } from '../Layouts/HomeLayout';
 
 const Header = () => {
-  const { userBalance } = useContext(UserBalanceContext);
-
-  const [users, setUsers] = useState([]);
-  const [viewBalance, setViewBalance] = useState(false);
-  const [loading, setLoading] = useState(true);
   const [openNavItem, setOpenNavItem] = useState(false);
-
-  const handleViewBalance = () => {
-    setViewBalance((prevViewBalance) => !prevViewBalance);
-  };
 
   const handleOpenNavitem = () => {
     setOpenNavItem((prevOpenNavItem) => !prevOpenNavItem);
@@ -36,36 +23,8 @@ const Header = () => {
   };
 
   useEffect(() => {
-    const token = localStorage.getItem('token');
-
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const userProfileResponse = await axios.get(
-          'https://take-home-test-api.nutech-integrasi.com/profile',
-          {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          }
-        );
-
-        setUsers(userProfileResponse.data.data);
-      } catch (err) {
-        console.error(err);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchData();
     handleResize();
   }, []);
-
-  const imageProfile =
-    users.profile_image !==
-    'https://minio.nutech-integrasi.com/take-home-test/null'
-      ? users.profile_image
-      : profileImageDefault;
 
   return (
     <>
@@ -79,7 +38,7 @@ const Header = () => {
           <div className=' hidden sm:flex items-center gap-x-10 font-medium text-end'>
             <NavItem title={'Top Up'} href={'/home/top-up'} />
             <NavItem title={'Transaction'} href={'/home/transaction'} />
-            <NavItem title={'Akun'} href={'/home/akun'} />
+            <NavItem title={'Akun'} href={'/home/account'} />
           </div>
           <GiHamburgerMenu
             onClick={handleOpenNavitem}
@@ -111,41 +70,12 @@ const Header = () => {
         />
         <NavItem
           title={'Akun'}
-          href={'/home/akun'}
+          href={'/home/account'}
           className={
             'rounded-lg border-2 bg-white h-10 flex items-center justify-center'
           }
         />
       </div>
-
-      {loading ? null : (
-        <section className='w-full px-4 xl:px-0 mt-5 flex flex-col gap-y-4 md:flex-row  md:max-w-7xl md:mx-auto'>
-          <div className='w-full flex flex-col gap-y-2'>
-            <img className='w-20' src={imageProfile} alt='' />
-            <div>
-              <h3>Selamat datang,</h3>
-
-              <h2 className='text-xl font-bold'>{`${users.first_name} ${users.last_name}`}</h2>
-            </div>
-          </div>
-
-          <div className=' w-full bg-red-500 text-white rounded-xl p-5 flex flex-col gap-y-2 justify-center'>
-            <p className='text-sm'>Saldo anda</p>
-            <h3 className='text-2xl font-semibold'>
-              Rp{' '}
-              {viewBalance ? userBalance.toLocaleString('id-ID') : '********'}
-            </h3>
-
-            <button
-              onClick={handleViewBalance}
-              className='text-sm flex items-center gap-x-3 max-w-fit'
-            >
-              <p>{viewBalance ? 'Tutup Saldo' : 'Lihat Saldo'}</p>
-              {viewBalance ? <FaEyeSlash /> : <FaEye />}
-            </button>
-          </div>
-        </section>
-      )}
     </>
   );
 };
